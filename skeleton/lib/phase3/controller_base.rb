f68edf3276188d66917c293cc2039bf981cfcd1a
App Academy Student <student@appacademy.io>
2015-05-19 15:12:08 -0700

    Finish phase 3: reading and evaluating templates

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -8,6 +8,14 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      raise Exception if already_built_response?
+      cn = self.class.to_s
+      controller_name = cn[0..cn.length-11].downcase
+      template = File.read("./views/#{controller_name}_controller/#{template_name}.html.erb")
+
+      evaluated_content=ERB.new(template).result(binding)
+
+      render_content(evaluated_content, "text/html")
     end
   end
 end
