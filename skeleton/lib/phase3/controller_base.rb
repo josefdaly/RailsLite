d22dc0addf6bcbcb94ca4f403c51e3a44ddc5d8e
App Academy Student <student@appacademy.io>
2015-05-20 14:21:37 -0700

    Finish Phase 6 and Phase 7

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -9,13 +9,13 @@ module Phase3
     # pass the rendered html to render_content
     def render(template_name)
       raise Exception if already_built_response?
-
       cn = self.class.to_s
       controller_name = cn[0..cn.length-11].downcase
-      
-      template = File.read("./views/#{controller_name}_controller/#{template_name}.html.erb")
+      template = File.read(
+        "./views/#{controller_name}_controller/#{template_name}.html.erb"
+      )
 
-      evaluated_content=ERB.new(template).result(binding)
+      evaluated_content = ERB.new(template).result(binding)
 
       render_content(evaluated_content, "text/html")
     end
